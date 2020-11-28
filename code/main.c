d4fe26f0aa1f1f1633d15e5233a3fe008ad2f696
Hajar-B <56651530+Hajar-B@users.noreply.github.com>
2020-11-28 16:15:14 +0100

    Update main.c

--- code/main.c
+++ code/main.c
@@ -17,6 +17,14 @@ void saveGraph(ListOfCities * cities){
   fclose(fileOut);
 }
 
+void saveGraph2(tas * cities){
+  FILE* fileOut = NULL;
+  fileOut = fopen("resuGraph.dat", "w");
+  for(int i=0; i<cities->nb_element; i++){
+    fprintf(fileOut, "%i %i\n", cities->tab[i].Ville_D, cities->tab[i].Ville_A);
+  }
+  fclose(fileOut);
+}
 
 int main(int argc, char ** argv) {
 
@@ -32,18 +40,19 @@ int main(int argc, char ** argv) {
   printf("%d\n", atoi(argv[1]));
 
   ListOfCities* cities;
-  tas* t;
+  tas* t, *graphe;
   arete* a;
   int nb_arete;
   char fichier[100] = "graphe";
   char extension[100] = ".dat";
   cities = citiesReader(popMin);
-
-
+  graphe = creer_tas((cities->number*(cities->number-1))/2);
+  /*
   for(int i=0; i<cities->number; i++){
     printf("%s %i %f %f\n", cities->name[i], cities->pop[i], cities->lon[i], cities->lat[i]);
-  }
-  float distance_total = kruskal_algo(cities, strcat(strcat(fichier, argv[1]), extension))
+  }*/
+  float distance_total = kruskal_algo(cities, strcat(strcat(fichier, argv[1]), extension), graphe);
+  affichage(graphe);
   printf("distance total : %f\n",distance_total);
 
 
@@ -58,7 +67,7 @@ int main(int argc, char ** argv) {
    Dans votre rendu, cette fonction prendra un graphe en argument,
    pas une liste de villes.
   */
-  saveGraph(cities);
+  saveGraph2(graphe);
 
 
   freeListOfCities(cities);
